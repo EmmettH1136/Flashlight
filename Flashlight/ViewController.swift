0231a65d9e135aafe589873431909390dc3702ba
Emmett Hasley <Ehasley1136@jh-mba158-06.local>
2019-02-12 11:37:11 -0600

    Flashlight

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
+var light = false
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.image = UIImage(named: "light-bulb")
+        // bam baby
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func whenButttonPressed(_ sender: UIButton) {
+//        print("Hello")
+        light = !light
+        if light {
+            view.backgroundColor = UIColor.black
+            button.setTitle("On", for: .normal)
+            button.setTitleColor(UIColor.green, for: .normal)
+            imageView.image = UIImage(named: "Lightbulb 4")
+            
+            
+        } else {
+            view.backgroundColor = UIColor.white
+            button.setTitle("Off", for: .normal)
+            button.setTitleColor(UIColor.blue, for: .normal)
+            imageView.image = UIImage(named: "light-bulb")
+            
+        }
+    }
 }
 
